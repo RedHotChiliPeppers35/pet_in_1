74fa888647c9b8ec76b9f59c92423b0066bd013e
Ata Berk Çinetçi <ata.berk.cinetci@icloud.com>
2023-07-05 01:22:16 +0300

    Changes to be committed: 	modified:   lib/Pages/Pet Owner/Advertize/add_advertize.dart

--- lib/Pages/Pet Owner/Advertize/add_advertize.dart
+++ lib/Pages/Pet Owner/Advertize/add_advertize.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
 
 class AddAdvertizePage extends StatefulWidget {
   const AddAdvertizePage({super.key});
@@ -11,6 +12,7 @@ class AddAdvertizePage extends StatefulWidget {
 
 class _AddAdvertizePageState extends State<AddAdvertizePage> {
   String? selectedAdres;
+  String? selectedPet;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,37 +21,94 @@ class _AddAdvertizePageState extends State<AddAdvertizePage> {
       ),
       body: SafeArea(
         child: Center(
-          child: StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection("users")
-                  .doc(FirebaseAuth.instance.currentUser!.uid)
-                  .collection("User Adress")
-                  .snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                  return const CircularProgressIndicator.adaptive();
-                } else {
-                  return DropdownButton(
-                    items: snapshot.data!.docs
-                        .map(
-                          (document) => DropdownMenuItem(
-                            child: Text(
-                              document["Title"],
-                            ),
-                            value: document.id,
-                          ),
-                        )
-                        .toList(),
-                    value: selectedAdres,
-                    onChanged: (newValue) {
-                      setState(() {
-                        print(newValue);
-                        selectedAdres = newValue;
-                      });
-                    },
-                  );
-                }
-              }),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Lütfen adres seçin"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection("users")
+                          .doc(FirebaseAuth.instance.currentUser!.uid)
+                          .collection("User Adress")
+                          .snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (!snapshot.hasData) {
+                          return const CircularProgressIndicator.adaptive();
+                        } else {
+                          return DropdownButton(
+                            iconEnabledColor: applicationPurple,
+                            iconDisabledColor: applicationOrange,
+                            items: snapshot.data!.docs
+                                .map(
+                                  (document) => DropdownMenuItem(
+                                    child: Text(
+                                      document["Title"],
+                                    ),
+                                    value: document.id,
+                                  ),
+                                )
+                                .toList(),
+                            value: selectedAdres,
+                            onChanged: (newValue) {
+                              setState(() {
+                                print(newValue);
+                                selectedAdres = newValue;
+                              });
+                            },
+                          );
+                        }
+                      }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Lütfen dostunuzu seçin"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection("users")
+                          .doc(FirebaseAuth.instance.currentUser!.uid)
+                          .collection("User Pets")
+                          .snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (!snapshot.hasData) {
+                          return const CircularProgressIndicator.adaptive();
+                        } else {
+                          return DropdownButton(
+                            iconEnabledColor: applicationPurple,
+                            iconDisabledColor: applicationOrange,
+                            items: snapshot.data!.docs
+                                .map(
+                                  (document) => DropdownMenuItem(
+                                    child: Text(
+                                      document["Pet Name"],
+                                    ),
+                                    value: document.id,
+                                  ),
+                                )
+                                .toList(),
+                            value: selectedPet,
+                            onChanged: (newValue) {
+                              setState(() {
+                                selectedPet = newValue;
+                              });
+                            },
+                          );
+                        }
+                      }),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
